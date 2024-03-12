cd65c670f2847bb9ba807bd2300362d52a52bbb2
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-03-13 00:09:49 +0530

    Add files via upload

---
+++ variables_4.py
@@ -0,0 +1,13 @@
+# variables data type in python
+a = 1,2,3,4,5,6   # integer values
+print(a)
+
+b = 1.23, 3.15, 5.65, 4.23    # float values
+print(b)
+
+c = True,False # Boolean values
+print(c)
+
+d = ("yoge","poorna","earth")  # string values
+print(d)
+
